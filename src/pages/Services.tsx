4c2966f95cb2bafa892dc85c5c0cf07ff8532b50
Wesley Thieko <wkumagai@inovvati.com.br>
2024-11-27 12:09:33 -0400

    feat: adicionado ada

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -6,10 +6,10 @@ import { Footer } from "../components/Footer";
 const Services = () => {
   const navigate = useNavigate();
     return (
-      <div className="relative bg-[#f5f5f5]">
-          <div className="w-full h-full relative overflow-hidden dark:bg-[#373737]">
+      <div className="relative bg-[#f5f5f5] dark:bg-[#373737]">
+          <div className="w-full h-full relative overflow-hidden ">
               <div className="flex w-full h-[1920px]  flex-col justify-between">
-                  <div className="w-full h-[380px] relative">
+                  <div className="w-full h-[350px] relative">
                     <img 
                       src='/images/logo.png'
                       alt="Logo"
@@ -46,8 +46,47 @@ const Services = () => {
                         <ServicosEmDestaque />
                       </div>
                   </div>
+                  <div className="px-32 py-8 pb-[112px]">
+                    <div className="w-full max-h-[430px] flex items-center justify-start flex-col relative gap-6">
+                      <div className="relative w-[182px] h-[45px] block">
+                          <img 
+                            src="/images/logo-ada.png"
+                            alt="Logo"
+                            data-nimg="fill"
+                            className="object-contain"
+                            />
+                      </div>
+                      <div className="pl-24">
+                        <div className="flex w-[620px] max-h-[180px] min-h-[150px] h-full border bg-white rounded-3xl shadow overflow-y-auto">
+                          <div className="text-black w-full h-full px-14 py-8 text-justify text-[17px] flex flex-col prose-strong:font-black prose-p:font-normal gap-2">
+                            <p>Olá! Eu sou a <strong>ADA</strong> e estou aqui para te ajudar.</p>
+                            <p>Se esta for a sua primeira vez usando o totem de autoatendimento e precisar de ajuda, clique no botão no cano inferior esquerdo para mais informações.</p>
+                          </div>
+                        </div>
+                      </div>
+                    <div className="absolute w-[230px] h-[330px] -bottom-8 -left-14 z-20">
+                      <img 
+                      src="/images/welcome-ada.png"
+                      alt="Logo"
+                      data-nimg="fill"
+                      className="object-contain"
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
               <Footer/>
+               <div
+                  className="absolute bottom-[108px] left-8 z-30 w-[120px] h-[100px]"
+                  onClick={() => navigate("/chatBot")}
+                >
+                  <img 
+                    src="/images/chat-ada.png"
+                    alt="Logo"
+                    data-nimg="fill"
+                    className="object-contain"
+                    />
+            </div>
           </div>
       </div>
     );
